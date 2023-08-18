1d60524fb3b44236ba824987892cf3c6d67f8b8e
lipun038 <lipun038@gmail.com>
2023-08-18 15:05:01 -0400

    Update express-dev.js

--- scripts/express-dev.js
+++ scripts/express-dev.js
@@ -7,7 +7,7 @@ const app = express();
 
 customServer(app);
 
-app.listen(3002, () => {
+app.listen(process.env.PORT || 3002, () => {
     // eslint-disable-next-line no-console
     console.log('Yay, local server started');
 });
